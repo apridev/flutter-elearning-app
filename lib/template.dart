193b7b3b2205fd519ffacf8dd48ed416c7249149
Apridev <apriantofajar11@gmail.com>
2022-05-06 23:57:48 +0900

    template

---
+++ lib/template.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 30;
+
+Color primaryColor = Color(0xff4483EB);
+Color blackColor = Color(0xff171717);
+Color backgroundColor = Color(0xffffffff);
+Color subtitelColor = Color(0xff8D8D8D);
+
+TextStyle primaryTextStyle = GoogleFonts.montserrat(
+  color: blackColor
+);
+
+TextStyle subtitleTextStyle = GoogleFonts.montserrat(
+  color: subtitelColor
+);
+
+TextStyle whiteTextStyle = GoogleFonts.montserrat(
+  color: backgroundColor
+);
+
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
