bf9bc9cd9b0f50a6066dad0bd3a4f6e33ed74c73
Apridev <apriantofajar11@gmail.com>
2022-05-10 14:07:18 +0900

    register page

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,20 @@
+import 'package:elearning_mobile_app/template.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Register Page')
+          ],
+        ),
+      ),
+    );
+  }
+}
