a04fc1a0aefd3082462b0aadc9e5157d8be6b52a
Apridev <apriantofajar11@gmail.com>
2022-05-10 14:06:39 +0900

    template

--- lib/template.dart
+++ lib/template.dart
@@ -6,8 +6,11 @@ double defaultMargin = 30;
 Color primaryColor = Color(0xff4483EB);
 Color blackColor = Color(0xff171717);
 Color backgroundColor = Color(0xffffffff);
+Color backgroundColor2 = Color(0xffFAFAFA);
 Color inputForm = Color(0xffD1D1D1);
 Color subtitelColor = Color(0xff8D8D8D);
+Color searchBorderColor = Color(0xffDEDEDE);
+Color iconNavColor = Color(0xffDEDEDE);
 
 TextStyle primaryTextStyle = GoogleFonts.montserrat(
   color: blackColor
