608611527631eda7ab3f1b269c57ca12fd92e8d4
Apridev <apriantofajar11@gmail.com>
2022-05-11 11:15:10 +0900

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:elearning_mobile_app/home/home_page.dart';
 import 'package:elearning_mobile_app/pages/login_page.dart';
 import 'package:elearning_mobile_app/pages/onboarding.dart';
+import 'package:elearning_mobile_app/pages/product_page.dart';
 import 'package:elearning_mobile_app/pages/register_page.dart';
 import 'package:flutter/material.dart';
 
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
         '/login_page' :(context) => LoginPage(),
         '/register_page' :(context) => RegisterPage(),
         '/home_page' :(context) => HomePage(),
+        '/product_page' :(context) => ProductPage(),
       },
     );
   }
